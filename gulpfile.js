92c0cf4c00626a4b24f9d2128138d271dd8c052a
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2014-07-04 11:16:46 +0800

    1. add bootstrap css into component Currently include the whole package, perhaps at later stages the bootstrap library can be separated into different packages

--- gulpfile.js
+++ gulpfile.js
@@ -11,5 +11,21 @@ gulp.task('copy-react-bootstrap', function () {
 	}
 });
 
-gulp.task('default', ['copy-react-bootstrap'], function () {
+gulp.task('copy-bootstrap-css', function () {
+	if(fs.existsSync(__dirname + '/node_modules/bootstrap')){
+		gulp.src([__dirname + '/node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+		__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css'])
+			.pipe(gulp.dest('bootstrap/css'));
+	}
+});
+gulp.task('copy-bootstrap-fonts', function () {
+	if(fs.existsSync(__dirname + '/node_modules/bootstrap')){
+		gulp.src(__dirname + '/node_modules/bootstrap/dist/fonts/*')
+			.pipe(gulp.dest('bootstrap/fonts'));
+	}
+});
+
+
+
+gulp.task('default', ['copy-react-bootstrap', 'copy-bootstrap-css', 'copy-bootstrap-fonts'], function () {
 });
